75dcc7181a2bb8297b58db8c597c87a71ce8155d
oono <andychao3210@gmail.com>
2020-10-29 13:53:42 +0800

    2020.10.29 commits

--- mybatis_onetoone/src/com/atguigu/mapper/KeyMapper.java
+++ mybatis_onetoone/src/com/atguigu/mapper/KeyMapper.java
@@ -8,6 +8,18 @@ import com.atguigu.pojo.Key;
  */
 public interface KeyMapper {
 
-    public Key queryKeyById(Integer id);
+    /**
+     * 根据Key编号查询key[pojo]信息（常用列）
+     * @param id：key的编号
+     * @return
+     */
+    Key queryKeyById(Integer id);
+
+    /**
+     * 两次查询，一次查key（常用列表），一次查lock（不常用表）
+     * @param id：key的编号
+     * @return
+     */
+    Key queryKeyByIdForTwoSteps(Integer id);
 
 }
