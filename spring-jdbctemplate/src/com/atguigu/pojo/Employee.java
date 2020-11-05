38d76df795446eee1d9766d7ac7c3bd1de765402
oono <andychao3210@gmail.com>
2020-11-05 14:25:43 +0800

    2020.11.5 commits

---
+++ spring-jdbctemplate/src/com/atguigu/pojo/Employee.java
@@ -0,0 +1,56 @@
+package com.atguigu.pojo;
+
+import java.math.BigDecimal;
+
+/**
+ * @author oono
+ * @date 2020 11 05
+ */
+public class Employee {
+
+    private Integer id;
+    private String name;
+    private BigDecimal salary;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public Employee(Integer id, String name, BigDecimal salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Employee() {
+    }
+}
---
+++ spring-jdbctemplate/src/com/atguigu/tx/dao/EmployeeDao.java
@@ -0,0 +1,8 @@
+package com.atguigu.tx.dao;
+
+/**
+ * @author oono
+ * @date 2020 11 05
+ */
+public class EmployeeDao {
+}
